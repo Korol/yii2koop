44e895de0dc5f9e5e770272de49724bbd833044e
Andrey Korol <andkorol@gmail.com>
2017-03-02 16:06:15 +0200

    Products: new no-image placeholder path

--- config/web.php
+++ config/web.php
@@ -35,7 +35,7 @@ $config = [
             'imagesCachePath' => 'upload/cache', //path to resized copies
             'graphicsLibrary' => 'GD', //but really its better to use 'Imagick'
 //            'placeHolderPath' => '@webroot/images/placeHolder.png', // if you want to get placeholder when image not exists, string will be processed by Yii::getAlias
-            'placeHolderPath' => '@webroot/upload/store/no-image.png', // if you want to get placeholder when image not exists, string will be processed by Yii::getAlias
+            'placeHolderPath' => '@webroot/images/no-image.png', // if you want to get placeholder when image not exists, string will be processed by Yii::getAlias
         ],
     ],
     'components' => [
