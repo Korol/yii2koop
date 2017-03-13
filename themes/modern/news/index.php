b42ebe8aebf4fcc54c25d27d76665799170e01fe
Andrey Korol <andkorol@gmail.com>
2017-03-13 17:57:38 +0200

    News list: link for header

--- themes/modern/news/index.php
+++ themes/modern/news/index.php
@@ -17,7 +17,7 @@ use yii\widgets\LinkPager;
                 <div class="row">
                     <div class="col-sm-9">
                         <h3 class="news-list-header">
-                            <a class="news-list-title" href=""><?= $item['title']; ?></a>
+                            <a class="news-list-title" href="<?=\yii\helpers\Url::to(['/news/view', 'id' => $item['id'], 'slug' => $item['slug']]); ?>"><?= $item['title']; ?></a>
                         </h3>
                     </div>
                     <div class="col-sm-3">
